4808b73eac058bba3def27e1dc6fcb5ae00cd9d8
Diego Ferreira Gonçalves <ferreira.dfg@gmail.com>
2021-09-18 01:01:31 -0300

    routes in express

--- express/index.js
+++ express/index.js
@@ -6,14 +6,27 @@ app.get("/", function(req, res){
     // res.send("other") pode enviar um resposta uma unica vez 
     // nao podemos deixar de enviar alguma coisa na rota 
 });
-app.get("/blog", function(req,res){
-    res.send("<h1>Welcome to my blog</h1>")
+app.get("/blog/:article?", function(req,res){
+    // res.send("<h1>Welcome to my blog</h1>")
+    var article = req.params.article
+    if(article) {
+        res.send("<h2>Article: "+article+"</h2>")
+    }else{
+        re.send("<h2>Welcome to my blog</h2>")
+    }
 })
 
 app.get("/channel/youtube", function(req,res){
     res.send(" Welcome to my channel")
 })
 
+app.get("/hello/:name/:company",function(req,res){
+    //Req sao o dados enviado pelo usuario
+    //Res sao a resposta que serão enviada para o usuario 
+    var name = req.params.name;
+    var company = req.params.company
+    res.send("<h1>Hello "+name+" da "+company+"</h1>")
+})
 
 app.listen(4000,function(error){
     if(error){
